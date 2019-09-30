0eb323f1f3466d7e32bf0a4c0b53535c7f2d8814
mDoso <36088193+mDoso@users.noreply.github.com>
2019-09-29 21:36:52 -0400

    Fourth is mostly functional, runs out or memory it seems for larger arrays

--- pa1/fourth/fourth.c
+++ pa1/fourth/fourth.c
@@ -1,32 +1,74 @@
 #include <stdlib.h>
 #include <stdio.h>
 /*Standard include statements*/
+
 int main(int argc, char** argv)
 {
 	FILE* f = fopen(argv[1],"r");
-	int i, rowMat1, columnMat1, rowMat2, columnMat2;
-	char line[1000];
+	int i, j, k, numRow1, numColumn1, numRow2, numColumn2;
 	int** matrix1;
+	int** matrix2;
+	int** matrixProduct;
+
+
 	if(f != NULL)
 	{
-		fscanf(f, "%d %d", &rowMat1, &columnMat1);
-		//printf("%d %d\n", rowMat1, columnMat1);
-		matrix1 = (int **)malloc(rowMat1 * sizeof(int *));
-		for(i = 0; i < rowMat1; i++)
-		{
-			fgets(line, sizeof(line), f);
-			fscanf(f, "%d %d\n", &rowMat2, &columnMat2);
-			printf("%d %d\n", rowMat2, columnMat2);
-			matrix1[i] = (int *)malloc(columnMat1 * sizeof(int));
+		fscanf(f, "%d %d", &numRow1, &numColumn1);//read first line for rows and columns
+		matrix1 = (int **)malloc(numRow1 * sizeof(int *));//allocate memory for # of rows
+		for(i = 0; i < numRow1; i++)//loop to allocate memory for # of columns and also add data
+		{			
+			matrix1[i] = (int *)malloc(numColumn1 * sizeof(int));
+			for(j = 0; j < numColumn1; j++)
+			{
+				fscanf(f,"%d", &matrix1[i][j]);
+			}			
 		}
-		fscanf(f, "%d %d\n", &rowMat2, &columnMat2);
-		printf("%d %d\n", rowMat2, columnMat2);
+		fscanf(f, "%d %d", &numRow2, &numColumn2);//read number of rows and columns for matrix 2
+		if(numColumn1 != numRow2)//check if matrix will be possible
+		{
+			printf("bad-matrices\n");
+			fclose(f);
+			free(matrix1);
+			exit(0);
+		}
+		else
+		{
+			matrix2 = (int **)malloc(numRow2 * sizeof(int *));//allocate memory for matrix 2 rows
+			matrixProduct = (int **)malloc(numRow1 * sizeof(int *));//allocate memory for matrix product rows
+			for(i = 0; i < numRow2; i++)//loop to allocate memory for # of columns and also add data
+			{			
+				matrix2[i] = (int *)malloc(numColumn2 * sizeof(int));
+				for(j = 0; j < numColumn2; j++)
+				{
+					fscanf(f,"%d", &matrix2[i][j]);
+				}				
+			}
+			for(i = 0; i < numRow1; i++)
+			{
+				matrixProduct[i] = (int *)malloc(numColumn2 * sizeof(int));
+				for(j = 0; j < numColumn1; j++)
+				{
+					matrixProduct[i][j]=0;
+					for(k = 0; k < numColumn2; k++)
+						matrixProduct[i][j] = matrixProduct[i][j] + matrix1[i][k] * matrix2[k][j];
+				}
+			}
+			for(i = 0; i < numRow1; i++)
+			{
+				for(j = 0; j < numColumn2; j++)
+				{
+					printf("%d\t", matrixProduct[i][j]);
+				}
+				printf("\n");
+			}
+			free(matrix1);
+			free(matrix2);
+			free(matrixProduct);
+		}		
 	}
 	else
 	{
 		printf("Could not open file.\n");
         return 1;
 	}
-
-	fclose(f);
 }
